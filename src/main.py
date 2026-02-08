f62da12c14f784a3c347a20f7b686abca75af632
Evgenii Petrov IceBook <epoy74@gmail.com>
2026-02-08 12:14:42 +0400

    refactor(main): remove empty strings and consolidate logic into one function

--- src/main.py
+++ src/main.py
@@ -1,41 +1,39 @@
-def get_data_from_input_str(
-    input_val: str,
-) -> tuple[int, list[tuple[int, int]]]:
-    """
-    Docstring for get_data_from_input_str
-
-    :param input: Description
-    :type input: str
-    :return: Description
-    :rtype: list[int]
-    """
-    try:
-        input_list = list(map(int, input_val.strip().split()))
-    except ValueError as err:
-        raise err
-    input_count = int(input_list.pop(0))
-
-    parsed_input = [
-        (int(input_list[i]), int(input_list[i + 1]))
-        for i in range(0, len(input_list), 2)
-    ]
-
-    return (input_count, parsed_input)
-
-
 def main(input_data: str) -> str:
-    current_water_volume_l: int = 0
-    water_flow_rate: int = 1
-    d_time: int = 0
+    """
+    Calculate how much water will be in the humidifier after the last refill.
+    Between refills, the water level decreases by 1 unit per 1 unit of time.
+    The water level cannot become negative.
 
+    Parameters:
+    input_data: (str) - input data in the format:
+    - First line: integer N — number of refills
+    - Next N lines: two integers T_i and V_i — time and refill volume
+
+    Example input:
+    4   - number of refills
+    1 3 : 1 - refill time, 3 - refill water
+    3 1
+    etc
+
+    Output:
+    (str) - final water volume
+    """
+    current_water_volume_l: int = 0
+    d_time: int = 0
     past_water_time: int = 0
     water_time: int = 0
     d_water: int = 0
-
     water_volume_l: int = 0
+    try:
+        input_list = list(map(int, input_data.strip().split()))
+    except ValueError as err:
+        raise err
+    input_list.pop(0)
 
-    num_water, water = get_data_from_input_str(input_data)
-
+    water = [
+        (int(input_list[i]), int(input_list[i + 1]))
+        for i in range(0, len(input_list), 2)
+    ]
     for index, (water_time, water_volume_l) in enumerate(water):
         if index == 0:
             # d_water = water_volume_l
@@ -44,10 +42,10 @@ def main(input_data: str) -> str:
             d_time = water_time - past_water_time
             if d_time >= current_water_volume_l:
                 current_water_volume_l = 0
+                d_water = 0
             else:
-                d_water = current_water_volume_l - (d_time * water_flow_rate)
+                d_water = current_water_volume_l - d_time
             current_water_volume_l = water_volume_l + d_water
+        d_water = 0
         past_water_time = water_time
-
-    print(current_water_volume_l)
     return str(current_water_volume_l)
